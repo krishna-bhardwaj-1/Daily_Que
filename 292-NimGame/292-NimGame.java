42b2b6ffe383a25c1d6aa6ba7a3e66ae53cdac60
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-12 00:18:34 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 12/08/2025, 00:18:34
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        }
+        return true;
+    }
+}
