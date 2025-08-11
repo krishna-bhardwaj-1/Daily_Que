be2d8aa440c7b9d879af659dfd62123a6cd77727
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-12 00:13:04 +0530

    Create 2132-Convert1dArrayInto2dArray

---
+++ 2132-Convert1dArrayInto2dArray/2132-Convert1dArrayInto2dArray.java
@@ -0,0 +1,17 @@
+// Last updated: 12/08/2025, 00:13:04
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int[][] arr=new int[m][n];
+        if(original.length!=(m*n)){
+            return new int[0][0];
+        }
+        int k=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=original[k];
+                k++;
+            }
+        }
+        return arr;
+    }
+}
