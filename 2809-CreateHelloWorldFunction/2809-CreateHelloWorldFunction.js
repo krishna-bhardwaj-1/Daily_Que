1b2f8942d1d82e044b7c58aee4d0900d3a15c364
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-12 00:11:00 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 12/08/2025, 00:11:00
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function() {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
