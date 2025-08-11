989cf6e4a36d54009e933aeb2fd32044bb8184c3
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-12 00:14:22 +0530

    Create 1656-CountGoodTriplets

---
+++ 1656-CountGoodTriplets/1656-CountGoodTriplets.java
@@ -0,0 +1,16 @@
+// Last updated: 12/08/2025, 00:14:21
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int pair=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if((Math.abs(arr[i] - arr[j])<=a)){
+                    for(int k=j+1;k<arr.length;k++){
+                    if((Math.abs(arr[j] - arr[k])<=b)&&(Math.abs(arr[i] - arr[k])<=c)) pair++;
+                }
+                }
+            }
+        }
+        return pair;
+    }
+}
