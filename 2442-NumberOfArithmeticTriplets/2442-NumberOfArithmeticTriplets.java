a66aa3e10e45ef1225cd188efb2610dc55ab7654
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-12 00:12:01 +0530

    Create 2442-NumberOfArithmeticTriplets

---
+++ 2442-NumberOfArithmeticTriplets/2442-NumberOfArithmeticTriplets.java
@@ -0,0 +1,14 @@
+// Last updated: 12/08/2025, 00:12:01
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int c=0;
+        for(int i=0;i<nums.length-2;i++){
+            for(int j=i+1;j<nums.length-1;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    if((nums[j]-nums[i]==diff) &&(nums[k]-nums[j]==diff)) c++;
+                }
+            }
+        }
+        return c;
+    }
+}
