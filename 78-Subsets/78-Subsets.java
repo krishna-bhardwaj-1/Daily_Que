16e55782efeae0d2c018853975d8c75e407f377a
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-12 00:20:38 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.java
@@ -0,0 +1,20 @@
+// Last updated: 12/08/2025, 00:20:38
+class Solution {
+    static List<List<Integer>> ll;
+    public List<List<Integer>> subsets(int[] nums) {
+        ll=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
+        subset(nums,temp,0);
+        return ll;
+    }   
+    public void subset(int[] arr,List<Integer> ans,int idx){
+        if(idx==arr.length){
+            ll.add(new ArrayList<>(ans));
+            return;
+        }
+        subset(arr,ans,idx+1);
+        ans.add(arr[idx]);
+        subset(arr,ans,idx+1);
+        ans.removeLast();
+    }
+}
