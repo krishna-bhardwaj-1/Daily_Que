ff28e523d49859812aad6605ae54198bf092c89c
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-12 00:18:10 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,18 @@
+// Last updated: 12/08/2025, 00:18:09
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] arr=new int[26];
+        for(char ch:s.toCharArray()){
+            arr[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            arr[ch-'a']--;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                return (char)(i+97);
+            }
+        }
+        return ' ';
+    }
+}
