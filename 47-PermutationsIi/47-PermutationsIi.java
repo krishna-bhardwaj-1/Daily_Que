e47f882e21d122c00a3026ed4973941dd032a18b
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-12 00:21:17 +0530

    Create 47-PermutationsIi

---
+++ 47-PermutationsIi/47-PermutationsIi.java
@@ -0,0 +1,35 @@
+// Last updated: 12/08/2025, 00:21:17
+class Solution {
+    static List<List<Integer>> ans;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        ans=new ArrayList<>();
+        helper(new ArrayList<>(),nums);
+        return ans;
+    }
+
+    private void helper(List<Integer> ll,int[] nums){
+        if(nums.length==0){
+            if(!ans.contains(ll)) ans.add(new ArrayList<>(ll));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            int n=nums[i];
+            if(!isvalid(i+1,nums,n)){
+                int[] newarr=new int[nums.length-1];
+                int idx=0;
+                for(int j=0;j<nums.length;j++){
+                    if(j!=i) newarr[idx++]=nums[j];
+                }
+                ll.add(n);
+                helper(ll,newarr);
+                ll.remove(ll.size()-1);
+            }
+        }
+    }
+    private boolean isvalid(int i,int[] arr,int n){
+        for(;i<arr.length;i++){
+            if(arr[i]==n) return true;
+        }
+        return false;
+    }
+}
