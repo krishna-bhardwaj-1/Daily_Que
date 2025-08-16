900007c81b6494a76179677bc1a8191bda3c24a7
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-16 22:33:28 +0530

    Create 1323-Maximum69Number

---
+++ 1323-Maximum69Number/1323-Maximum69Number.java
@@ -0,0 +1,23 @@
+// Last updated: 16/08/2025, 22:33:27
+class Solution {
+    public int maximum69Number (int num) {
+        List<Integer> ll=new ArrayList<>();
+        while(num>0){
+            ll.add(num%10);
+            num/=10;
+        }
+
+        for(int i=ll.size()-1;i>=0;i--){
+            if(ll.get(i)==6){
+                ll.set(i,9);
+                break;
+            }
+        }
+
+        int sum=0;
+        for(int i=ll.size()-1;i>=0;i--){
+            sum=sum*10+ll.get(i);
+        }
+        return sum;
+    }
+}
