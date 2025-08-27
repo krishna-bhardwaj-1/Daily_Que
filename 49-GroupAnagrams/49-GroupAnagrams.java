bf9d623e6da988f00208b56ed41ffb5f444a1229
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-28 00:27:39 +0530

    File updated at 28/08/2025, 00:27:38

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,27 +1,30 @@
-// Last updated: 12/08/2025, 00:21:12
+// Last updated: 28/08/2025, 00:27:38
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String , List<String>>  map=new HashMap<>();
-        for(String currString:strs){
-            char[] currStringArray=currString.toCharArray();
-            Arrays.sort(currStringArray);
-            String sortedString=new String(currStringArray);
-            // System.out.println(currString +"->"+sortedString);
-            if(map.containsKey(sortedString)){
-                List<String> anagramlist=map.get(sortedString);
-                anagramlist.add(currString);
-                map.put(sortedString,anagramlist);
-            }
-            else {
-                List<String> anagrmlist=new ArrayList<>();
-                anagrmlist.add(currString);
-                map.put(sortedString,anagrmlist);
-            }
-        }
-        // System.out.println(map);
-
         List<List<String>> ans=new ArrayList<>();
-        ans.addAll(map.values());
+        Map<String,List<String>> map=new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String key=key(strs[i]);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(strs[i]);
+        }
+        for(String key:map.keySet()){
+            ans.add(map.get(key));
+        }
         return ans;
+    }
+
+    private String key(String s){
+        int[] fre=new int[26];
+        for(int i=0;i<s.length();i++){
+            fre[s.charAt(i)-'a']++;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i:fre){
+            sb.append(i+" ");
+        }
+        return sb.toString();
     }
 }
