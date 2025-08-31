f90299b92b025d999c80cf04709e79d336c94feb
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-08-31 22:03:24 +0530

    File updated at 31/08/2025, 22:03:24

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,9 +1,9 @@
-// Last updated: 12/08/2025, 00:21:25
+// Last updated: 31/08/2025, 22:03:24
 class Solution {
     public void solveSudoku(char[][] board) {
-        sudoku(board,0,0);
+        helper(board,0,0);
     }
-    private static boolean sudoku(char[][] board,int row,int col){
+    private boolean helper(char[][] board,int row,int col){
         if(col==9){
             row++;
             col=0;
@@ -12,38 +12,38 @@ class Solution {
             return true;
         }
         if(board[row][col]!='.'){
-            return sudoku(board,row,col+1);
+            return helper(board,row,col+1);
         }
-        else {
+        else{
             for(int val=1;val<=9;val++){
-                if(isItSafe(board,row,col,val)){
+                if(isitsafe(board,row,col,(char)(val+'0'))){
                     board[row][col]=(char)(val+'0');
-                    boolean ans=sudoku(board,row,col+1);
-                    if(ans){
-                        return true;
-                    }
+                    if(helper(board,row,col+1)) return true;
                     board[row][col]='.';
                 }
             }
         }
         return false;
     }
-    private static boolean isItSafe(char[][] board,int row,int col,int val){
+    private boolean isitsafe(char[][] board,int row,int col,char val){
+        //row
         for(int i=0;i<9;i++){
-            if(board[i][col]==(char)(val+'0')){
+            if(board[row][i]==val){
                 return false;
             }
         }
-        for (int i=0;i<9;i++){
-            if(board[row][i]==(char)(val+'0')){
+        //col
+        for(int i=0;i<9;i++){
+            if(board[i][col]==val){
                 return false;
             }
         }
-        int cr=row-row%3;
-        int cc=col-col%3;
-        for (int i=cr;i<cr+3;i++){
-            for (int j=cc;j<cc+3;j++){
-                if(board[i][j]==(char)(val+'0')){
+
+        row=row-row%3;
+        col=col-col%3;
+        for(int i=row;i<row+3;i++){
+            for(int j=col;j<col+3;j++){
+                if(board[i][j]==val){
                     return false;
                 }
             }
