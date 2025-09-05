2354375f9fba988dc0dec78725099a33cfb40a19
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-09-05 20:26:18 +0530

    File updated at 05/09/2025, 20:26:17

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,22 +1,34 @@
-// Last updated: 12/08/2025, 00:19:20
+// Last updated: 05/09/2025, 20:26:17
 class Solution {
     public int rob(int[] nums) {
-        int[] cache=new int[nums.length+1];
-        Arrays.fill(cache,-1);
-        return(maxMoney(nums,0,nums.length,cache));
+        // int[] dp=new int[nums.length];
+        // Arrays.fill(dp,-1);
+        // return helper(nums,0,dp);
+        return RobberBU(nums);
     }
-    private static int maxMoney(int[] house,int curr,int n,int[] cache){
-        if(curr>=n){
-            return 0;
+    // private int helper(int[] arr,int curr,int[] dp){
+    //     if(curr>=arr.length){
+    //         return 0;
+    //     }
+    //     if(dp[curr]!=-1){
+    //         return dp[curr];
+    //     }
+    //     int c1=arr[curr]+helper(arr,curr+2,dp);
+    //     int c2=helper(arr,curr+1,dp);
+
+    //     return dp[curr]=Math.max(c1,c2);
+    // }
+
+    private int RobberBU(int[] arr){
+        if(arr.length==1) return arr[0];
+        int[] dp=new int[arr.length];
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<arr.length;i++){
+            int c1=arr[i]+dp[i-2];
+            int c2=dp[i-1];
+            dp[i]=Math.max(c1,c2);
         }
-        if(cache[curr]!=-1){
-            return cache[curr];
-        }
-        //loot liya
-        int c1=house[curr]+maxMoney(house,curr+2,n,cache);
-        //mat looto
-        int c2=maxMoney(house,curr+1,n,cache);
-        cache[curr]=Math.max(c1,c2);
-        return Math.max(c1,c2);
+        return dp[dp.length-1];
     }
 }
